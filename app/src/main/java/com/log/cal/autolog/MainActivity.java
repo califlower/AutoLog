3b6987b03a4e8432126b4792412d161918fe3667
Cal <cal13199@gmail.com>
2016-02-09 21:59:05 -0500

    Major UI changes

--- app/src/main/java/com/log/cal/autolog/MainActivity.java
+++ app/src/main/java/com/log/cal/autolog/MainActivity.java
@@ -1,6 +1,8 @@
 package com.log.cal.autolog;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +13,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,20 +29,57 @@ public class MainActivity extends AppCompatActivity
         /*
             Holds all the objects
          */
-        List<Bike_Object> bike_array_list=new ArrayList<>();
-        Bike_Object Test_Bike=new Bike_Object("Suzuki", "DRZ400SM", 2006, 20000);
-        bike_array_list.add(Test_Bike);
+        List<Bike_Object> bike_array_list;
+        //Bike_Object Test_Bike=new Bike_Object("Suzuki", "DRZ400SM", 2006, 20000);
+        //bike_array_list.add(Test_Bike);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /********************
+         * handles the saving part
+         * retrieves vehicles
+         */
+        Context context=MainActivity.this;
+        final SharedPreferences sharedPref = context.getSharedPreferences(getString(R.string.vehicle_array_preferences), Context.MODE_PRIVATE);
+        String vehicle_gson_array=sharedPref.getString(getString(R.string.vehicle_key),"empty_key");
+
+        /******
+         * Checks if the saved array is empty or not
+         */
+
+        if (vehicle_gson_array.compareTo("empty_key")==0)
+        {
+            bike_array_list=new ArrayList<>();
+        }
+        else
+        {
+            Gson gson=new Gson();
+            Type collectionType = new TypeToken<ArrayList<Bike_Object>>(){}.getType();
+            ArrayList<Bike_Object> temp_list=gson.fromJson(vehicle_gson_array,collectionType);
+            bike_array_list=temp_list;
+        }
+
 
         Bundle inc=getIntent().getExtras();
 
 
         if (inc!=null && inc.get("bike_year")!=null && inc.get("bike_make")!=null && inc.get("bike_model")!=null)
         {
+
+            Gson gson=new Gson();
+
             Bike_Object new_bike=new Bike_Object(inc.get("bike_make").toString(),inc.get("bike_model").toString(),Integer.parseInt(inc.get("bike_year").toString()),Integer.parseInt(inc.get("bike_mile").toString()));
             bike_array_list.add(new_bike);
+
+            SharedPreferences.Editor editor = sharedPref.edit();
+
+            String insert_preference=gson.toJson(bike_array_list);
+
+            editor.putString(getString(R.string.vehicle_key),insert_preference);
+
+            editor.apply();
+
         }
 
 
