753bbeff2663e1f580967ab0a578edabce210a86
Cal <cal13199@gmail.com>
2016-02-09 23:06:42 -0500

    Allows removal of cards Cards can now be removed, shared preferences now work too

--- app/src/main/java/com/log/cal/autolog/Bike_Card_Adapter.java
+++ app/src/main/java/com/log/cal/autolog/Bike_Card_Adapter.java
@@ -1,11 +1,18 @@
 package com.log.cal.autolog;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bike_Card_Adapter extends RecyclerView.Adapter<Bike_Card_Adapter.Bike_Card_ViewHolder>
@@ -34,6 +41,7 @@ public class Bike_Card_Adapter extends RecyclerView.Adapter<Bike_Card_Adapter.Bi
         holder.bike_model.setText(b.bike_model);
         holder.bike_year.setText(String.valueOf(b.bike_year));
         holder.bike_mile.setText(String.valueOf(b.bike_mileage));
+
     }
 
     @Override
@@ -48,6 +56,8 @@ public class Bike_Card_Adapter extends RecyclerView.Adapter<Bike_Card_Adapter.Bi
         final TextView bike_model;
         final TextView bike_year;
         final TextView bike_mile;
+        final TextView update_miles;
+        final TextView remove_bike;
 
         public Bike_Card_ViewHolder(View v)
         {
@@ -57,6 +67,58 @@ public class Bike_Card_Adapter extends RecyclerView.Adapter<Bike_Card_Adapter.Bi
             bike_model=(TextView) v.findViewById(R.id.card_model);
             bike_year=(TextView) v.findViewById(R.id.card_year);
             bike_mile=(TextView) v.findViewById(R.id.card_mileage);
+            update_miles=(TextView) v.findViewById(R.id.update_miles);
+            remove_bike=(TextView) v.findViewById(R.id.remove_bike);
+
+            remove_bike.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+
+
+                    /****************
+                     * Handles the issue of not removing saved objects
+                     */
+                    Context context=v.getContext();
+                    final SharedPreferences sharedPref = context.getSharedPreferences(context.getString(R.string.vehicle_array_preferences), Context.MODE_PRIVATE);
+                    String vehicle_gson_array=sharedPref.getString(context.getString(R.string.vehicle_key),"empty_key");
+
+                    /******
+                     * Checks if the saved array is empty or not
+                     */
+
+                    if (vehicle_gson_array.compareTo("empty_key")!=0)
+                    {
+                        Gson gson=new Gson();
+                        Type collectionType = new TypeToken<ArrayList<Bike_Object>>(){}.getType();
+                        ArrayList<Bike_Object> temp_list=gson.fromJson(vehicle_gson_array,collectionType);
+
+                        /*******
+                         * Removes item from recyclerview and sharedpreferences
+                         */
+                        temp_list.remove(getAdapterPosition());
+                        bikes.remove(getAdapterPosition());
+                        notifyItemRemoved(getAdapterPosition());
+
+                        SharedPreferences.Editor editor = sharedPref.edit();
+                        String insert_preference=gson.toJson(temp_list);
+
+                        editor.putString(context.getString(R.string.vehicle_key),insert_preference);
+
+                        editor.apply();
+
+                        /****
+                         * applies changes
+                         */
+                    }
+
+
+
+
+                }
+            });
+
 
         }
     }
